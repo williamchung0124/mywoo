7442b97214bba3cdd06d1b57a6f7722bcf7b51e3
“jiamin” <jiamin@gritworld.com>
2025-06-30 01:45:20 +0800

    code

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-06-04 22:19:40
  * @LastEditors: “jiamin” “jiamin@gritworld.com”
- * @LastEditTime: 2025-06-28 15:48:08
+ * @LastEditTime: 2025-06-30 01:43:08
  * @FilePath: /mywoo/assets/js/header.js
  */
 function createNavMenu(menuItems, menuContainerId) {
@@ -42,15 +42,88 @@ function setActiveMenuItem(menuItems, menuContainerId) {
   });
 }
 
+
+
+
+
 const menuItems = [
-  { id: 1, name: "首页", link: "home.html" },
-  { id: 2, name: "關於我們", link: "about.html" },
-  { id: 3, name: "服務項目", link: "serve.html" },
-  { id: 4, name: "菁英群", link: "eliteGroup.html" },
-  { id: 5, name: "房產大小事", link: "news.html" },
-  { id: 6, name: "法拍專業課程", link: "class.html" },
-  { id: 7, name: "資訊會員登入", link: "vipLogin.html" },
+  {
+    id: '1',
+    name: "首頁",
+    link: "home.html",
+  },
+  {
+    id: 2,
+    name: "關於我們",
+    link: "about.html",
+  },
+  {
+    id: 3,
+    name: "服務項目",
+    link: "serve.html",
+  },
+  {
+    id: 4,
+    name: "菁英群",
+    link: "eliteGroup.html",
+  },
+  {
+    id: 5,
+    name: "房產大小事",
+    link: "news.html",
+  },
+  {
+    id: 6,
+    name: "法拍專業課程",
+    link: "class.html",
+  },
+  {
+    id: 7,
+    name: "資訊會員登入",
+    link: "vipLogin.html",
+  },
 ];
 
 createNavMenu(menuItems, "nav-menu");
 setActiveMenuItem(menuItems, "nav-menu");
+
+
+const currentPath = window.location.pathname.split("/").pop();
+const searchParams = window.location.search;
+
+if (currentPath === "list.html") {
+  const navMenu = document.getElementById("nav-menu");
+  if (navMenu) {
+    const firstLi = navMenu.querySelector("li:first-child");
+    if (firstLi) {
+      firstLi.innerHTML = `
+        <a href="./home.html">首頁</a> <i>/<i>
+        <a href="./list.html${searchParams}">查询结果</a>
+      `;
+    }
+  }
+} else if (currentPath === "list-content.html") {
+  const navMenu = document.getElementById("nav-menu");
+  if (navMenu) {
+    const firstLi = navMenu.querySelector("li:first-child");
+    if (firstLi) {
+      firstLi.innerHTML = `
+        <a href="./home.html">首頁</a> <i>/<i>
+        <a href="./list.html${searchParams}">查詢結果</a> <i>/<i>
+        <a href="./list-content.html${searchParams}">詳細資料</a>
+      `;
+    }
+  }
+} else if (currentPath === "list-map.html") {
+  const navMenu = document.getElementById("nav-menu");
+  if (navMenu) {
+    const firstLi = navMenu.querySelector("li:first-child");
+    if (firstLi) {
+      firstLi.innerHTML = `
+        <a href="./home.html">首頁</a> <i>/<i>
+        <a href="./list-map.html${searchParams}">地圖搜尋</a>
+      `;
+    }
+  }
+}
+
